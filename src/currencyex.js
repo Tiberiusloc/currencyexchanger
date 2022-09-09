92f1d76fca6912df8fa3ba04ead282093794c149
Tiberius Lockett <tlockett680@gmail.com>
2022-09-09 09:32:50 -0700

    add currencyex.js and apicall

---
+++ src/currencyex.js
@@ -0,0 +1,15 @@
+export class CurrencyExchange {
+  static async getCurrency(currency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}}/latest/${currency}`);
+      const jsonResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`
+        throw new Error(errorMessage);
+      }
+      return jsonResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { CurrencyExchange } from './currencyex';
 
 // Business Logic
 
+async function getCurrency(currency) {
+  const response = await CurrencyExchange.getCurrency(currency);
+  if (response.main) {
+    printElements(currency)
+  } else {
+    printError(currency)
+  }
+}
 
 
 // UI Logic
 
+function printElements(currency) {
+  document.querySelector('#showResponse').innerText = `This currency is ${currency}`
+}
+
+function printError(currency, error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the currency data for ${currency}:${error}`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const currency = document.querySelector('#currency').value;
+  document.querySelector('#currency').value = null;
+  getCurrency(currency);
+}
+
+window.addEventListener("load", function(){
+  this.document.querySelector('form').addEventListener('submit', handleFormSubmission);
+})
