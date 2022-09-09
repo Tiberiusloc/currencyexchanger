e36094ccaffaaeb909e814eadb12af63d2fae436
Tiberius Lockett <tlockett680@gmail.com>
2022-09-09 16:40:30 -0700

    add Working project, images, css styles

--- src/index.js
+++ src/index.js
@@ -16,13 +16,13 @@ function handleFormSubmission(event) {
   event.preventDefault();
   const currency = document.querySelector('#currency').value;
   CurrencyExchange.getCurrency(currency)
-  .then(function(response) {
-    const resp = response;
-    let rate = resp.conversion_rates.USD;
-    let usd = document.querySelector('#userNumber1').value;
-    document.querySelector('#conversion1').innerText =  "The currency converted from USD to " + currency + " is " + convert(usd,rate);
-    document.querySelector('#currency').value = null;
-    document.querySelector('#userNumber1').value = null;
+    .then(function(response) {
+      const resp = response;
+      let rate = resp.conversion_rates.USD;
+      let usd = document.querySelector('#userNumber1').value;
+      document.querySelector('#conversion1').innerText =  "The currency converted from USD to " + currency + " is " + convert(usd,rate);
+      document.querySelector('#currency').value = null;
+      document.querySelector('#userNumber1').value = null;
     });
 }
 
@@ -30,18 +30,17 @@ function handleAnySubmission(event) {
   event.preventDefault();
   const currency = document.querySelector('#currency2').value;
   CurrencyExchange.getCurrency(currency)
-  .then(function(response) {
-    console.log(response)
-    const resp = response;
-    let changeRate = document.querySelector('#currencyChange').value;
-    let rate = resp.conversion_rates[changeRate]
-    console.log(rate);
-    let anyCurr = document.querySelector('#userNumber2').value;
-    document.querySelector('#conversion2').innerText =  "The currency converted from " + currency + " to " + changeRate + " is " + backConvert(anyCurr,rate);
-    document.querySelector('#currency2').value = null;
-    document.querySelector('#userNumber2').value = null;
-    document.querySelector('#currencyChange').value = null;
-  });
+    .then(function(response) {
+      const resp = response;
+      let changeRate = document.querySelector('#currencyChange').value;
+      let rate = resp.conversion_rates[changeRate];
+      console.log(rate);
+      let anyCurr = document.querySelector('#userNumber2').value;
+      document.querySelector('#conversion2').innerText =  "The currency converted from " + currency + " to " + changeRate + " is " + backConvert(anyCurr,rate);
+      document.querySelector('#currency2').value = null;
+      document.querySelector('#userNumber2').value = null;
+      document.querySelector('#currencyChange').value = null;
+    });
 
 }
 
